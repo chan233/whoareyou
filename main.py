19d6cc512787e918d8ca90b7f2aa7455f6dfe7b1
chan <chan@codeserver.com>
2021-12-07 15:22:52 +0800

    start xray analysis

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello world")
+import argparse
+import os 
+
+def clear(): 
+    return os.system('cls' if os.name == 'nt' else 'clear')
+print("")
+
+title = '''
+            |                                                                         
+\ \  \   /  __ \    _ \   __|   _ \        _` |   __|   _ \       |   |   _ \   |   | 
+ \ \  \ /   | | |   __/  |      __/       (   |  |      __/       |   |  (   |  |   | 
+  \_/\_/   _| |_| \___| _|    \___|      \__._| _|    \___|      \__. | \___/  \__._| 
+                                                                 ____/                
+
+
+
+  V0.1
+
+'''
+print(title)
+print("")
+parser = argparse.ArgumentParser("katana-ds.py",formatter_class=argparse.RawTextHelpFormatter)
+
+
+parser.add_argument("-x","--xary", help="Analysis Xray log", action='store_true' )
+args = parser.parse_args()
+
+if args.xary :
+ clear() 
+ from Analysis import xraylog
+ 
+
